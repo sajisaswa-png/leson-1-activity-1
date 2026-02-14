e4d7a23eec2b0e58c83f90c0dd8e08c6b2b0fc3f
codercode1010 <codercode1010@gmail.com>
2026-02-14 08:27:15 +0530

    COMMIT

---
+++ #Print a Science.py
@@ -0,0 +1,12 @@
+#Print a Science
+print("Welcome to the world of program")
+#Print a Number
+
+
+print(4)
+
+
+#Next Line after print
+print("Hello Python \n")
+#Pass multiple arguments for printing print("hello",5)
+#End argument of Print Statement print("welcome to ", end="*")
