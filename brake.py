cf9e6774b1841f0ae8c243c38c3ba563592766da
sajisaswa-png <sajis.aswa@gmail.com>
2026-07-18 11:13:37 +0530

    initial.commit

---
+++ brake.py
@@ -0,0 +1,10 @@
+a = input("enter a word : ")
+for i in a :
+ if i == "e":
+
+ 
+       print("e is found")
+       break
+else:
+         print("e is not found")   
+
---
+++ continu.py
@@ -0,0 +1,9 @@
+var = 15
+
+while var > 0 :
+    var = var - 1
+    if var == 5:
+            continue
+            
+    print('\n current variable value:',var)
+print("goodbye")
